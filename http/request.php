e0a195fac50f46e3af9fe1a87968a9bad2978a6e
Richard Aas <richard@db.org>
2016-09-21 09:50:28 +0200

    http/request: Added getParam() method

--- http/request.php
+++ http/request.php
@@ -8,7 +8,7 @@ class request
 	public $client;
 	public $method;
 	public $path;
-	public $params;
+	public $param;
 	public $host;
 	public $content;
 
@@ -21,7 +21,7 @@ class request
 		$this->client = $_SERVER["REMOTE_ADDR"];
 		$this->method = strtolower($_SERVER["REQUEST_METHOD"]);
 		$this->path   = $path ? $path : $uri;
-		$this->params = $_REQUEST;
+		$this->param  = (object)$_REQUEST;
 		$this->host   = $_SERVER["HTTP_HOST"];
 
 		if (isset($_SERVER["CONTENT_TYPE"])) {
@@ -31,7 +31,7 @@ class request
 			switch ($this->content->type) {
 
 			case "application/json":
-				$this->params = (array)json_decode(file_get_contents("php://input"));
+				$this->param = json_decode(file_get_contents("php://input"));
 				break;
 			}
 		}
@@ -40,11 +40,22 @@ class request
 
 	function get($key, $placeholder=NULL)
 	{		
-		if (isset($this->params[$key]))
-			return $this->params[$key];
+		if (property_exists($this->param, $key))
+			return $this->param->$key;
 		else if (func_num_args() == 1)
 			throw new \Exception("$key must be provided", 400);
 		else
 			return $placeholder;
+	}
+
+
+	function getParam($key, &$value=NULL)
+	{
+		if (!property_exists($this->param, $key))
+			return false;
+
+		$value = $this->param->$key;
+
+		return true;
 	}
 }
