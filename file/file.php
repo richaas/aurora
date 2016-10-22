a5360d381b8e01bf4b2bba483b42b5102df4283f
Richard Aas <richaraas@gmail.com>
2016-10-22 12:54:31 +0200

    added filesystem functions

---
+++ file/file.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace aurora\file;
+
+
+class file
+{
+	public static function link($target, $link)
+	{
+		if (!link($target, $link))
+			throw new \Exception("link($target, $link): " . error_get_last()["message"]);
+	}
+
+
+	public static function mkdir($pathname, $mode=0777, $recursive=false)
+	{
+		if (!mkdir($pathname, $mode, $recursive))
+			throw new \Exception("mkdir($pathname): " . error_get_last()["message"]);
+	}
+
+
+	public static function scandir($pathname, $sorting_order=SCANDIR_SORT_ASCENDING)
+	{
+		$ret = scandir($pathname, $sorting_order);
+		if ($ret === false)
+			throw new \Exception("scandir($pathname): " . error_get_last()["message"]);
+
+		return $ret;
+	}
+
+
+	public static function unlink($filename)
+	{
+		if (!file_exists($filename))
+			return;
+
+		if (!unlink($filename))
+			throw new \Exception(error_get_last()["message"]);
+	}
+}
