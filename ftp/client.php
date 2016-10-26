b798d0a76370a269c1db0b9fd13953ee878daa89
Richard Aas <richaraas@gmail.com>
2016-10-26 16:18:58 +0200

    added ftp client

---
+++ ftp/client.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace aurora\ftp;
+
+
+class client
+{
+	private $curl;
+
+
+	public function __construct()
+	{
+		$this->curl = curl_init();
+		if (!$this->curl)
+			throw new \Exception("curl_init(): failed");
+	}
+
+
+	public function __destruct()
+	{
+		curl_close($this->curl);
+	}
+
+
+	public function setVerifyPeer($verify)
+	{
+		curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, $verify);
+	}
+
+
+	public function setCredentials($username, $password)
+	{
+		curl_setopt($this->curl, CURLOPT_USERPWD, $username . ":" . $password);
+	}
+
+
+	public function getInfo()
+	{
+		return curl_getinfo($this->curl);
+	}
+
+
+	public function put($filename, $url)
+	{
+		$fp = fopen($filename, "rb");
+		if (!$fp)
+			throw new \Exception(error_get_last()["message"]);
+
+		curl_setopt($this->curl, CURLOPT_URL, $url);
+		curl_setopt($this->curl, CURLOPT_UPLOAD, true);
+		curl_setopt($this->curl, CURLOPT_INFILE, $fp);
+
+		$res = curl_exec($this->curl);
+
+		fclose($fp);
+
+		if ($res === false)
+			throw new \Exception(curl_error($this->curl));
+	}
+}
