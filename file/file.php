bbb70dea1d7e1253135e595ef59d327693da37c3
Richard Aas <richaraas@gmail.com>
2016-11-05 12:47:17 +0100

    added file

---
+++ file/file.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace aurora\file;
+
+
+class file
+{
+	public $fp;
+
+
+	public function __construct($filename, $mode="r")
+	{
+		$this->fp = fopen($filename, $mode);
+		if ($this->fp === false)
+			throw new \Exception(error_get_last()["message"]);
+	}
+
+
+	public function __destruct()
+	{
+		fclose($this->fp);
+	}
+
+
+	public function seek($offset, $whence=SEEK_SET)
+	{
+		if (fseek($this->fp, $offset, $whence) !== 0)
+			throw new \Exception(error_get_last()["message"]);
+	}
+
+
+	public function read($length)
+	{
+		$data = fread($this->fp, $length);
+		if ($data === false)
+			throw new \Exception(error_get_last()["message"]);
+
+		return $data;
+	}
+
+
+	public function write($data)
+	{
+		if (fwrite($this->fp, $data) === false)
+			throw new \Exception(error_get_last()["message"]);
+	}
+
+
+	public function truncate($size)
+	{
+		if (!ftruncate($this->fp, $size))
+			throw new \Exception(error_get_last()["message"]);
+	}
+
+
+	public function stat()
+	{
+		$stat = fstat($this->fp);
+		if (!$stat)
+			throw new \Exception(error_get_last()["message"]);
+
+		return (object)array_slice($stat, count($stat)/2);
+	}
+
+
+	public function lock($exclusive, $block=false)
+	{
+		$mode = $exclusive ? LOCK_EX : LOCK_SH;
+
+		if (!$block)
+			$mode |= LOCK_NB;
+
+		$ret = flock($this->fp, $mode, $wouldblock);
+		if (!$ret && !$wouldblock)
+			throw new \Exception(error_get_last()["message"]);
+
+		return $ret;
+	}
+
+
+	public function unlock()
+	{
+		if (!flock($this->fp, LOCK_UN))
+			throw new \Exception(error_get_last()["message"]);
+	}
+}
