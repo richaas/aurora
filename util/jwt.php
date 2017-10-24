ca48ade3ea415080b9e012b62487eac710a996ff
Richard Aas <richaraas@gmail.com>
2017-10-24 11:38:08 +0200

    jwt: added RS256 algorithm + api adjustments

--- util/jwt.php
+++ util/jwt.php
@@ -7,8 +7,10 @@ use Exception;
 
 class jwt
 {
-	private $alg;
-	private $jwt;
+	private $signature;
+	private $packet;
+	public $header;
+	public $payload;
 
 
 	public static function encode($payload, $key, $alg="HS256")
@@ -20,52 +22,74 @@ class jwt
 		switch ($alg) {
 
 		case "HS256":
-			$sig = base64url::encode(hash_hmac("sha256", $pkt, $key, true));
+			$sig = hash_hmac("sha256", $pkt, $key, true);
+			if ($sig === false)
+				throw new Exception(error_get_last()["message"]);
+			break;
+
+		case "RS256":
+			if (!openssl_sign($pkt, $sig, $key, "sha256"))
+				throw new Exception(error_get_last()["message"]);
 			break;
 
 		default:
-			throw new Exception("unsupported jwt algorithm: $alg");
+			throw new Exception("unsupported jwt algorithm: " . $alg);
 		}
 
-		return $pkt . "." . $sig;
+		return $pkt . "." . base64url::encode($sig);
 	}
 
 
-	public function decode($token)
+	public static function decode($token, $key)
+	{
+		$jwt = new jwt($token);
+
+		if (!$jwt->validate($key))
+			throw new Exception("invalid credentials", 400);
+
+		return $jwt->payload;
+	}
+
+
+	public function __construct($token)
 	{
 		$jwt = explode(".", $token);
 		if (count($jwt) !== 3)
-			return NULL;
+			throw new Exception("invalid token", 400);
 
-		$hdr = json_decode(base64url::decode($jwt[0]));
-		if ($hdr === NULL || !isset($hdr->alg))
-			return NULL;
+		$this->header = json_decode(base64url::decode($jwt[0]));
+		if ($this->header === NULL || !isset($this->header->alg))
+			throw new Exception("invalid token header", 400);
 
-		$pld = json_decode(base64url::decode($jwt[1]));
-		if ($pld === NULL)
-			return NULL;
+		$this->payload = json_decode(base64url::decode($jwt[1]));
+		if ($this->payload === NULL)
+			throw new Exception("invalid token payload", 400);
 
-		$this->alg = $hdr->alg;
-		$this->jwt = $jwt;
-
-		return $pld;
+		$this->signature = base64url::decode($jwt[2]);
+		$this->packet    = $jwt[0] . "." . $jwt[1];
 	}
 
 
 	public function validate($key)
 	{
-		$pkt = $this->jwt[0] . "." . $this->jwt[1];
-
-		switch ($this->alg) {
+		switch ($this->header->alg) {
 
 		case "HS256":
-			$sig = base64url::encode(hash_hmac("sha256", $pkt, $key, true));
-			break;
+			$sig = hash_hmac("sha256", $this->packet, $key, true);
+			if ($sig === false)
+				throw new Exception(error_get_last()["message"]);
+
+			return $sig === $this->signature;
+
+		case "RS256":
+			$res = openssl_verify($this->packet, $this->signature, $key, "sha256");
+			if ($res === false)
+				throw new Exception(error_get_last()["message"]);
+
+			return $res === 1;
 
 		default:
-			return false;
+			throw new Exception("unsupported jwt algorithm: " . $this->header->alg);
 		}
-
-		return ($this->jwt[2] === $sig);
 	}
 }
