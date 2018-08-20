80049a1c686ad890875c0ed05b4dd3debdac3e38
Richard Aas <richaraas@gmail.com>
2018-08-20 11:49:50 +0200

    aes: added raw argument and file methods

--- crypto/aes.php
+++ crypto/aes.php
@@ -2,6 +2,8 @@
 
 namespace aurora\crypto;
 
+use aurora\file\file;
+use aurora\file\util as futl;
 use aurora\util\rand;
 use Exception;
 
@@ -10,9 +12,10 @@ class aes
 {
 	const method = "aes-256-ctr";
 	const ivsize = 16;
+	const blocksize = 65536;
 
 
-	public static function encrypt($data, $key)
+	public static function encrypt($data, $key, $raw=false)
 	{
 		$iv = rand::bytes(self::ivsize);
 
@@ -22,13 +25,15 @@ class aes
 		if ($res === false)
 			throw new Exception("aes: unable to encrypt data");
 
-		return base64_encode($res . $iv);
+		$res .= $iv;
+
+		return $raw ? $res : base64_encode($res);
 	}
 
 
-	public static function decrypt($data, $key)
+	public static function decrypt($data, $key, $raw=false)
 	{
-		$raw = base64_decode($data);
+		$raw = $raw ? $data : base64_decode($data);
 
 		if (strlen($raw) < self::ivsize)
 			return false;
@@ -38,5 +43,30 @@ class aes
 		return openssl_decrypt(substr($raw, 0, -self::ivsize), self::method,
 				       hash("sha256", $key),
 				       OPENSSL_RAW_DATA, $iv);
+	}
+
+
+	public static function encryptFile($path, $key)
+	{
+		$src = new file($path, "r");
+		$dst = new file($path . "~", "w");
+
+		while (strlen($data = $src->read(self::blocksize - self::ivsize)))
+			$dst->write(self::encrypt($data, $key, true));
+
+		futl::rename($path . "~", $path . "_");
+	}
+
+
+	public static function decryptFile($path, $key)
+	{
+		$dstPath = rtrim($path, "_");
+		$src = new file($path, "r");
+		$dst = new file($dstPath . "~", "w");
+
+		while (strlen($data = $src->read(self::blocksize)))
+			$dst->write(self::decrypt($data, $key, true));
+
+		futl::rename($dstPath . "~", $dstPath);
 	}
 }
