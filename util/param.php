67cf9b95322b990491bc31affe58f1039c14d654
Richard Aas <richaraas@gmail.com>
2021-03-27 15:12:19 +0100

    Added util/param class

---
+++ util/param.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace aurora\util;
+
+use Exception;
+
+
+class param
+{
+	public $param;
+
+
+	public function __construct($param)
+	{
+		$this->param = (object)$param;
+	}
+
+
+	public function get($key, $placeholder=NULL)
+	{
+		if (property_exists($this->param, $key))
+			return $this->param->$key;
+		else if (func_num_args() == 1)
+			throw new Exception("parameter $key must be provided", 400);
+		else
+			return $placeholder;
+	}
+
+
+	public function getParam($key, &$value=NULL)
+	{
+		if (!property_exists($this->param, $key))
+			return false;
+
+		$value = $this->param->$key;
+
+		return true;
+	}
+}
