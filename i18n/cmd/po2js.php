b443e03d56d381b5cb83edeb355eae33b82d4547
Richard Aas <richaraas@gmail.com>
2021-05-04 19:59:53 +0200

    i18n: added console commands: po2php and po2js

---
+++ i18n/cmd/po2js.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace cmd\i18n;
+
+use aurora\file\util as futl;
+use Gettext\Loader\PoLoader;
+
+
+class po2js extends po2php
+{
+	const desc = "Create js translation from po";
+
+
+	protected function escape($str)
+	{
+		return str_replace(["\n", "\r"], ["\\n", "\\r"], parent::escape($str));
+	}
+
+
+	public function exec($poFile, $jsFile="php://stdout")
+	{
+		$loader = new PoLoader();
+
+		$trans = @$loader->loadFile($poFile);
+
+		$lang     = basename($jsFile, ".js");
+		$plurForm = $trans->getHeaders()->getPluralForm();
+		$nplurals = (int)($plurForm[0] ?? 2);
+		$plural   = $this->checkPlural($plurForm[1] ?? "n != 1");
+		$msgs     = "";
+
+		foreach ($trans->getTranslations() as $tr) {
+
+			if ($tr->isDisabled())
+				continue;
+
+			$ptrs = $tr->getPluralTranslations();
+
+			$id  = $this->escape($tr->getOriginal());
+			$msg = $this->escape($tr->getTranslation());
+
+			$msgs .= "\n\t'$id': ";
+
+			if (count($ptrs) > 0) {
+
+				$msgs .= "['$msg'";
+
+				foreach ($ptrs as $ptr) {
+
+					$msg = $this->escape($ptr);
+
+					$msgs .= ", '$msg'";
+				}
+
+				$msgs .= "],";
+			}
+			else {
+				$msgs .= "'$msg',";
+			}
+		}
+
+		@futl::file_put_contents($jsFile, <<<EOT
+export default {
+	'': {
+		'language': '$lang',
+		'plural-forms': 'nplurals=$nplurals; plural=$plural;'
+	},$msgs
+};
+
+EOT);
+	}
+}
---
+++ i18n/cmd/po2php.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace cmd\i18n;
+
+use aurora\file\util as futl;
+use Exception;
+use Gettext\Loader\PoLoader;
+
+
+class po2php
+{
+	const desc = "Create php translation from po";
+
+
+	protected function checkPlural($plural)
+	{
+		if (preg_match("/[^0-9n\s!=<>()%&|?:]/", $plural) !== 0)
+			throw new Exception("invalid chars in plural formula: $plural");
+
+		return $plural;
+	}
+
+
+	protected function escape($str)
+	{
+		return str_replace(["\\", "'", "\x00"], ["\\\\", "\\'", ""], $str);
+	}
+
+
+	public function exec($poFile, $phpFile="php://stdout")
+	{
+		$loader = new PoLoader();
+
+		$trans = @$loader->loadFile($poFile);
+
+		$class  = basename($phpFile, ".php");
+		$plural = $this->checkPlural($trans->getHeaders()->getPluralForm()[1] ?? "n != 1");
+		$plural = str_replace("n", "\$num", $plural);
+		$msgs   = "";
+
+		foreach ($trans->getTranslations() as $tr) {
+
+			if ($tr->isDisabled())
+				continue;
+
+			$ptrs = $tr->getPluralTranslations();
+
+			$id  = $this->escape($tr->getOriginal());
+			$msg = $this->escape($tr->getTranslation());
+
+			$msgs .= "\n\t\t'$id' => ";
+
+			if (count($ptrs) > 0) {
+
+				$msgs .= "['$msg'";
+
+				foreach ($ptrs as $ptr) {
+
+					$msg = $this->escape($ptr);
+
+					$msgs .= ", '$msg'";
+				}
+
+				$msgs .= "],";
+			}
+			else {
+				$msgs .= "'$msg',";
+			}
+		}
+
+		@futl::file_put_contents($phpFile, <<<EOT
+<?php
+
+namespace lang;
+
+
+class $class
+{
+	public \$msgs = [$msgs
+	];
+
+
+	public function plural(\$num)
+	{
+		return $plural;
+	}
+}
+
+EOT);
+	}
+}
