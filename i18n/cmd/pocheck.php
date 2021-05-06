0f1bcdfac56e46eaa69f44d8a734f258709f7f4f
Richard Aas <richaraas@gmail.com>
2021-05-06 08:42:50 +0200

    i18n: added console command: pocheck

---
+++ i18n/cmd/pocheck.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace cmd\i18n;
+
+use Exception;
+use Gettext\Loader\PoLoader;
+
+
+class pocheck
+{
+	const desc = "Check po translations against template";
+
+	private $errc  = 0;
+	private $warnc = 0;
+
+
+	private function error($file, $id, $msg)
+	{
+		echo "$file: \x1b[31m$id\x1b[0m ($msg)\n";
+		$this->errc++;
+	}
+
+
+	private function warn($file, $id, $msg)
+	{
+		echo "$file: \x1b[33m$id\x1b[0m ($msg)\n";
+		$this->warnc++;
+	}
+
+
+	private function check($ref, $def, $file)
+	{
+		foreach ($ref->getTranslations() as $tr) {
+
+			if ($tr->isDisabled())
+				continue;
+
+			$id  = $tr->getOriginal();
+			$_tr = $def->find(NULL, $id);
+
+			if ($_tr === NULL || $_tr->isDisabled())
+				$this->error($file, $id, "missing");
+			else if ($_tr->getPlural() !== $tr->getPlural())
+				$this->error($file, $id, "plural mismatch");
+		}
+
+		foreach ($def->getTranslations() as $tr) {
+
+			if ($tr->isDisabled())
+				continue;
+
+			$id  = $tr->getOriginal();
+			$_tr = $ref->find(NULL, $id);
+
+			if ($_tr === NULL || $_tr->isDisabled())
+				$this->warn($file, $id, "obsolete");
+		}
+	}
+
+
+	public function exec($potFile, ...$poFiles)
+	{
+		$loader = new PoLoader();
+
+		$ref = @$loader->loadFile($potFile);
+
+		foreach ($poFiles as $poFile) {
+
+			$def = @$loader->loadFile($poFile);
+
+			$this->check($ref, $def, $poFile);
+		}
+
+		if ($this->errc > 0)
+			throw new Exception("errors: $this->errc, warnings: $this->warnc");
+
+		if ($this->warnc > 0)
+			echo "warnings: $this->warnc\n";
+	}
+}
