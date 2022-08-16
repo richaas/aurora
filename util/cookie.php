2c3af7b89b1275c73c97cdd5cd1c19a7a1e5358d
Richard Aas <richaraas@gmail.com>
2022-08-16 15:26:50 +0200

    Added util/cookie class

---
+++ util/cookie.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace aurora\util;
+
+
+class cookie
+{
+	public static function set($name, $value="", $lifetime=0, $path="", $samesite="Strict",
+				   $domain="", $secure=true, $httponly=true)
+	{
+		return setcookie($name, $value, ["expires"  => $lifetime ? time() + $lifetime : 0,
+						 "path"     => $path,
+						 "samesite" => $samesite,
+						 "domain"   => $domain,
+						 "secure"   => $secure,
+						 "httponly" => $httponly]);
+	}
+}
